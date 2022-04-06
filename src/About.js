5b7a106231cb97156a2461aac283ccbf23a6062f
PremKumarRTrainee <fixed-term.r.premkumar@in.bosch.com>
2022-04-06 15:56:56 +0530

    3 files modified, 2 added

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          BGSW restaurant is available with both veg and non-veg foods in a
+          meaningfull price with high hygiene and freshness.
+        </h2>
+      </div>
+    );
+  }
+}
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link, NavLink, Switch} from 'react-router-dom'
+import Home from "./Home"
+import About from "./About"
 
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <h1>REACT ROUTING</h1>
+      <Route path="/h" component={Home}/>
+      <Route path="/a" component={About}/>
+    </div>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById("root"));
